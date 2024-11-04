8366b6978550383fdd3152663f98991d02dfdb69
Valentin JEANDOT <valentin.jeandot@cirly.com>
2024-11-04 15:24:44 +0100

    remove react default page content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import { FC, useEffect } from 'react';
-import logo from '../logo.svg';
 import '../App.css';
 import { useTranslation } from 'react-i18next';
 import { useConfig } from '../contexts/ConfigContext';
@@ -9,20 +8,8 @@ const Home: FC = () => {
 
   const config = useConfig();
 
-  useEffect(() => {
-    console.log(config);
-  }, [config]);
-
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-
       <h1 className='mt-5'>{tHome('config')}</h1> 
       <p>{config.get<string>("config.value")}</p>
       <p>{config.get<string>("config.a.name")} : {config.get<number>("config.a.index")}</p>
