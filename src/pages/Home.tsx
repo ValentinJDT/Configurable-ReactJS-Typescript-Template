8b5a3e9f29076a7da1b017b389f73b4835bbb626
Valentin JEANDOT <valentin.jeandot@cirly.com>
2024-11-12 12:50:16 +0100

    add table sub page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { FC, useEffect } from 'react';
 import '../App.css';
 import { useTranslation } from 'react-i18next';
 import { useConfig } from '../contexts/ConfigContext';
+import Tables from './Tables';
 
 const Home: FC = () => {
   const [tHome] = useTranslation('home');
@@ -9,14 +10,17 @@ const Home: FC = () => {
   const config = useConfig();
 
   return (
-    <header className="App-header">
-      <h1 className="mt-5">{tHome('config')}</h1>
-      <p>{config.get<string>('config.value')}</p>
-      <p>
-        {config.get<string>('config.a.name')} : {config.get<number>('config.a.index')}
-      </p>
-      <p>{config.get<string[]>('config.a.nexts').length}</p>
-    </header>
+    <>
+      <header className='w-full flex flex-col items-center'>
+        <h1 className="mt-5">{tHome('config')}</h1>
+        <p>{config.get<string>('config.value')}</p>
+        <p>
+          {config.get<string>('config.a.name')} : {config.get<number>('config.a.index')}
+        </p>
+        <p>{config.get<string[]>('config.a.nexts').length}</p>
+      </header>
+      <Tables />
+    </>
   );
 };
 
